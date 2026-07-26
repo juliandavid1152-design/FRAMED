ae137eafe36d6e7fc7b11cdddb0c3e4dbe63275f
juliandavid1152-design <juliandavid1152@gmail.com>
2026-07-26 16:05:31 -0500

    Agrega estructura de procedimientos almacenados

---
+++ sql/05_procedimientos.sql
@@ -0,0 +1,25 @@
+/* =========================================================
+   PROYECTO FRAMED
+   Archivo: 05_procedimientos.sql
+   Descripción: Procedimientos almacenados del sistema
+   Motor: Oracle Database
+   ========================================================= */
+
+
+/* =========================================================
+   PROCEDIMIENTOS
+   ========================================================= */
+
+-- Registrar establecimiento
+
+-- Registrar reporte
+
+-- Registrar novedad
+
+-- Actualizar establecimiento
+
+-- Consultar historial
+
+-- Cambiar estado de reporte
+
+
