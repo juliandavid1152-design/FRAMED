ed43dc1c2c7681c5d4239535b858e9a39d756675
juliandavid1152-design <juliandavid1152@gmail.com>
2026-07-26 16:06:43 -0500

    Agrega estructura para datos de prueba

---
+++ sql/07_datos_prueba.sql
@@ -0,0 +1,21 @@
+/* =========================================================
+   PROYECTO FRAMED
+   Archivo: 07_datos_prueba.sql
+   Descripción: Datos para pruebas del sistema
+   Motor: Oracle Database
+   ========================================================= */
+
+
+/* =========================================================
+   DATOS DE PRUEBA
+   ========================================================= */
+
+-- Directores técnicos
+
+-- Establecimientos
+
+-- Reportantes
+
+-- Reportes
+
+-- Novedades
